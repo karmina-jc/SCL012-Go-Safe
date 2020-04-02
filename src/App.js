25ff1e39247f7fd9db7f18e141fd30e8fb28bdae
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-04-02 18:23:31 -0300

    ingresando inicios de register signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from  './templates/home';
 import MyRoutes from './templates/myRoutes';
 import HomeRoutes from './templates/homeRoutes';
 import logoApp from  './templates/pictures/Logo.jpeg';
+import SignUp from '../src/templates/signUp';
 //import Login from './templates/login';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,7 +14,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
     return (   
       <div>
-        <Home />
+        <SignUp />
         
       </div> 
       );
